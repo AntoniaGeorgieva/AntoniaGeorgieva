2d18c7796e30aaf6c1235f7b38bb973fb6938afb
Toni Georgieva <antonia2311@abv.bg>
2023-10-26 16:42:05 +0300

    Version 1

---
+++ Data/Models/Post.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace project1.Data.Models
+{
+    public class Post
+    {
+        [Required]
+        public string Title { get; set; } = null!;
+
+        [Required]
+        public string Description { get; set; } = null!;
+
+        [Required] 
+        public DateTime Date { get; set; }
+
+        [Required]
+        public string Creator { get; set; } = null!;
+
+        [Required]
+        public List<string> SkillsRequired { get; set; } = null!;
+    }
+}
---
+++ Models/ViewModels/RegisterFormModel.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations;
+using RequiredAttribute = Microsoft.Build.Framework.RequiredAttribute;
+
+namespace project1.Models.ViewModels
+{
+    public class RegisterFormModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; } = null!;
+
+        [Display(Name = "Firm Name (optional)")]
+        [StringLength(20, MinimumLength = 2)]
+        public string FirmName { get; set; } = null!;
+
+        [Required]
+        [Display(Name = "Name")]
+        [StringLength(20, MinimumLength = 2)]
+        public string Name { get; set; } = null!;
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; } = null!;
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; } = null!;
+    }
+}
